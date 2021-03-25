1c14ea845499aa853b3e2f8bbdbe20b8921a8ffe
Richard <sand.dossantos@live.fr>
2021-03-25 10:01:39 +0100

    Tested in multiple of 3 and 5 returns "FizzBuzz" (success)

--- FizzBuzzValueChecker.py
+++ FizzBuzzValueChecker.py
@@ -5,12 +5,15 @@ class FizzBuzzValueChecker:
     def checkValue(value):
         if value == 0 or value < 0:
             raise(IncorrectValueError())
-        elif value % 3 == 0: 
+
+        if value % 3 == 0 and value % 5 == 0:
+            return "FizzBuzz"
+
+        if value % 3 == 0: 
             return "Fizz"
         elif value % 5 == 0:
             return "Buzz"
-        elif value % 3 == 0 and value % 5 == 0:
-            return "FizzBuzz"
+            
 
 class IncorrectValueError(Exception):
     def __init__(self):
