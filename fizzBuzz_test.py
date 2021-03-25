2c3341fd41fef2ac9cadaeeb0ff58d0c2253535f
Richard <sand.dossantos@live.fr>
2021-03-25 09:52:34 +0100

    Tested if multiple of 3 returns "Fizz" (failed)

--- fizzBuzz_test.py
+++ fizzBuzz_test.py
@@ -10,3 +10,8 @@ class FizzBuzzTest(unittest.TestCase):
     def test_negative_raises_error(self):
         value = -1
         self.assertRaises(NegativeValueError, FizzBuzzValueChecker.checkValue, value)
+
+    def test_multiple_of_3_returns_Fizz(self):
+        value = 3
+        actual = FizzBuzzValueChecker.checkValue(value)
+        self.assertEqual("Fizz", actual)
